efb389074e70a365d1366d62be74ede361adcb9d
eduardoacunacoronado <edu191191@gmail.com>
2025-04-07 20:07:07 +0200

    Initial commit.

---
+++ app/src/main/java/com/example/androidmaster/Variables.kt
@@ -0,0 +1,17 @@
+package com.example.androidmaster
+
+fun main() {
+    print("Hola");
+
+    val intExample:Int = 1;
+    val longExample:Long = 1;
+    val floatExample:Float = 1.5f;
+    val doubleExample:Double = 1.5;
+
+    val charExample:Char = 'a';
+    val stringExample:String = "aaaaaaaa";
+
+    val booleanExample:Boolean = true;
+
+    print(stringExample);
+}
