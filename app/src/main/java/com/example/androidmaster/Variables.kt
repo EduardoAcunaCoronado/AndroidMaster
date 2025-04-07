bccaf36e613e20301362ff70577ee904b5ae35c4
eduardoacunacoronado <edu191191@gmail.com>
2025-04-07 20:39:48 +0200

    Variables.

--- app/src/main/java/com/example/androidmaster/Variables.kt
+++ app/src/main/java/com/example/androidmaster/Variables.kt
@@ -1,17 +1,38 @@
 package com.example.androidmaster
 
 fun main() {
-    print("Hola");
+    println("Hola");
 
-    val intExample:Int = 1;
+    val intExample:Int = 30;
     val longExample:Long = 1;
     val floatExample:Float = 1.5f;
     val doubleExample:Double = 1.5;
 
     val charExample:Char = 'a';
-    val stringExample:String = "aaaaaaaa";
+    val stringExample:String = "1";
 
     val booleanExample:Boolean = true;
 
-    print(stringExample);
+    var intExample2:Int = 29;
+    intExample2 = 29;
+
+    println("Sumar: ")
+    println(intExample + intExample2)
+
+    println("Restar: ")
+    println(intExample - intExample2)
+
+    println("Multiplicar: ")
+    println(intExample * intExample2)
+
+    println("Dividir: ")
+    println(intExample / intExample2)
+
+    println("Modulo: ")
+    println(intExample % intExample2)
+
+    println(stringExample);
+    var stringExample2 = "Hola"
+    stringExample2 = "Hola, me llamo ${stringExample.toInt()}"
+    println(stringExample2)
 }
