5c4c40935504aa4e7ec58f3941aacb4dadb299d8
eduardoacunacoronado <edu191191@gmail.com>
2025-04-09 01:12:23 +0200

    Menu.

---
+++ app/src/main/java/com/example/androidmaster/MenuActivity.kt
@@ -0,0 +1,40 @@
+package com.example.androidmaster
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.androidmaster.firstapp.FirstAppActivity
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu)
+
+        val btnSaludApp = findViewById<Button>(R.id.btnSaludApp)
+        val btnIMCApp = findViewById<Button>(R.id.btnIMCApp)
+        btnSaludApp.setOnClickListener { navigateToSaludApp () }
+        btnIMCApp.setOnClickListener { navigateToIMCApp() }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun navigateToSaludApp () {
+        val intent = Intent(this, FirstAppActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun navigateToIMCApp () {
+        val intent = Intent(this, FirstAppActivity::class.java)
+        startActivity(intent)
+    }
+}
+
